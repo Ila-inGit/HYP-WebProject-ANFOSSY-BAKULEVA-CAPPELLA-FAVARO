76c44d339590ba7bcac528e20f1d47be2930e018
marziaf <maarzia.f@gmail.com>
2021-04-25 10:24:40 +0200

    missing files

---
+++ components/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div>
+      <TopbarLogo />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ components/topbar/menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="menu">
+    <div v-for="item in sections" :key="item.key">
+      <h1>{{ item.displayText }}</h1>
+      <div v-for="elem in item.content" :key="elem">
+        <p>{{ elem }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    sections: [
+      {
+        key: 'about',
+        displayText: 'About us',
+        content: ['placeholder', 'placeholder', 'placeholder'],
+      },
+      {
+        key: 'areas',
+        displayText: 'Areas',
+        content: ['placeholder', 'placeholder', 'placeholder'],
+      },
+      {
+        key: 'products',
+        displayText: 'Products',
+        content: ['placeholder', 'placeholder', 'placeholder'],
+      },
+      {
+        key: 'people',
+        displayText: 'People',
+        content: ['placeholder', 'placeholder', 'placeholder'],
+      },
+    ],
+  }),
+}
+</script>
+
+<style>
+.menu {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
