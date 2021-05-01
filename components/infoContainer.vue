825836464b1eefc68cf61640291a81614da3e7d1
marziaf <maarzia.f@gmail.com>
2021-05-01 19:34:20 +0200

    as usual I forgot to add files in previous commits...

---
+++ components/infoContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="infoSection">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String },
+    description: { type: String },
+  },
+}
+</script>
+
+<style>
+.infoSection {
+  background: #444;
+  color: white;
+  padding: 5vh;
+}
+</style>
---
+++ pages/productPresentation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <info-container
+      v-bind:title="product.title"
+      v-bind:description="product.longDescription"
+    />
+  </div>
+</template>
+
+<script>
+import db from 'static/fake_db.json'
+
+export default {
+  layout: 'PageLayout',
+
+  data() {
+    let product = {
+      title: "Please, don't play with URLs...",
+      descrpition: '',
+      image: '',
+    }
+    db.forEach((element) => {
+      if (element.id === this.$router.currentRoute.query.productId) {
+        product = element
+      }
+    })
+    return { product: product }
+  },
+}
+</script>
