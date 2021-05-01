f444143cf94aa332e49800e8975c1043a4126bcb
marziaf <maarzia.f@gmail.com>
2021-05-01 12:57:47 +0200

    simple list of products

---
+++ components/products/productCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="card">
+    <div class="cardInfo">
+      <h1>{{ product.title }}</h1>
+      <p>{{ product.description }}</p>
+      <button-with-text v-bind:title="'more'" class="cardTitle" />
+    </div>
+    <img v-bind:src="product.image" alt="product image" class="imageCard" />
+  </div>
+</template>
+
+<script>
+import ButtonWithText from '../ButtonWithText.vue'
+export default {
+  components: { ButtonWithText },
+  props: {
+    product: {
+      title: {
+        type: String,
+        default: '',
+      },
+      description: {
+        type: String,
+        default: '',
+      },
+      image: {
+        type: String,
+        default: '',
+      },
+    },
+  },
+}
+</script>
+
+<style>
+.card {
+  display: flex;
+  height: auto;
+  width: auto;
+  padding-right: 5%;
+  padding-left: 5%;
+  padding-top: 1%;
+  padding-bottom: 1%;
+  border-style: solid;
+  border: 1% px #999;
+  border-radius: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 1%;
+}
+
+.cardInfo {
+  display: flex;
+  flex-direction: column;
+  height: auto;
+}
+
+.cardTitle {
+  display: flex;
+  justify-content: center;
+}
+
+.imageCard {
+  height: 20vh;
+  width: auto;
+}
+</style>
---
+++ pages/products.vue
@@ -0,0 +1,51 @@
+export
+<template>
+  <div>
+    <h1>Look at all those products!</h1>
+    <product-card
+      v-for="product in products"
+      :key="product"
+      v-bind:product="product"
+    />
+  </div>
+</template>
+
+<script>
+import productCard from '~/components/products/productCard.vue'
+export default {
+  components: {
+    productCard,
+  },
+  layout: 'PageLayout',
+  data: () => ({
+    // TODO: replace with DB
+    products: [
+      {
+        title: 'Pebbles',
+        description:
+          "Amazing tool to create circles on water.\nYes, it's plenty on the river, but ours are better.",
+        image: 'https://picsum.photos/seed/234/300/300',
+        page: 'link',
+      },
+      {
+        title: 'Dogs',
+        description: "Don't you love dogs?",
+        image: 'https://picsum.photos/seed/57/300/300',
+        page: 'link',
+      },
+      {
+        title: 'Bananas, a bit too ripe',
+        description: 'You can still remove the dark parts',
+        image: 'https://picsum.photos/seed/3467/300/300',
+        page: 'link',
+      },
+    ],
+  }),
+}
+</script>
+
+<style>
+.div {
+  width: 100%;
+}
+</style>
