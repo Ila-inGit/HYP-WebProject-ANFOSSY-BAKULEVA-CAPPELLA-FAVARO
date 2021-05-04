199e26356e47b68b1499ce234549f3d91b627a74
bakulevalena <bakuleva.lena@gmail.com>
2021-05-04 08:44:29 +0200

    add working tabs

--- components/people/PeopleTabs.vue
+++ components/people/PeopleTabs.vue
@@ -1,124 +1,34 @@
 <template>
-  <div class="tabs">
-    <ul class="tabs-list">
-      <li class="active" @onclick="openPart('Presentation')">
-        <a href="#Presentation">Presentation</a>
-      </li>
-      <li @onclick="openPart('Working Areas')">
-        <a href="#Working Areas" @onclick="openPart('Working Areas')">
-          Working Areas
-        </a>
-      </li>
-      <li @onclick="openPart('Developed Products')">
-        <a href="#Developed Products" @onclick="openPart('Developed Products')">
-          Developed Products
-        </a>
-      </li>
-    </ul>
-    <div id="Presentation" class="tab active">
-      <h3>Presentation</h3>
-      <p>here will be something</p>
-    </div>
-    <div id="Working Areas" class="tab">
-      <h3>Working Areas</h3>
-      <p>here will be something</p>
-    </div>
-    <div id="Developed Products" class="tab">
-      <h3>Tab3</h3>
-      <p>here will be something</p>
-    </div>
+  <div class="container">
+    <tabs>
+      <tab name="Presentation" :selected="true">
+        <h3>Presentation</h3>
+      </tab>
+      <tab name="Working Areas">
+        <div>
+          <h3>Working Areas</h3>
+        </div>
+      </tab>
+      <tab name="Developed Products">
+        <h3>Developed products</h3>
+      </tab>
+    </tabs>
   </div>
 </template>
 
 <script>
+import Tab from '~/components/tabs/tab'
+import Tabs from '~/components/tabs/tabs'
 export default {
-  methods: {
-    openPart(part) {},
+  components: {
+    Tabs,
+    Tab,
   },
 }
 </script>
 
 <style scoped>
-.tabs {
-  width: 60%;
-  height: auto;
-  margin: 0 auto;
-}
-
-/* tab list item */
-.tabs .tabs-list {
-  list-style: none;
-  margin: 0px;
-  padding: 0px;
-}
-
-.tabs .tabs-list li {
-  width: 150px;
-  float: left;
-  margin: 0px 2px 0px 0px;
-  padding: 10px 5px;
-  text-align: center;
-  background-color: #fafafc;
-  border-radius: 3px;
-}
-
-.tabs .tabs-list li:hover {
-  cursor: pointer;
-}
-
-.tabs .tabs-list li a {
-  text-decoration: none;
-  color: #1c1b1b;
-}
-
-/* Tab content section */
-.tabs .tab {
-  display: none;
-  width: 100%;
-  min-height: 250px;
-  height: auto;
-  border-radius: 3px;
-  padding: 20px 15px;
-  background-color: #fdfdff;
-  color: darkslategray;
-  clear: both;
-}
-
-.tabs .tab h3 {
-  border-bottom: 3px solid #1b1b1c;
-  letter-spacing: 1px;
-  font-weight: normal;
-  padding: 5px;
-}
-
-.tabs .tab p {
-  line-height: 20px;
-  letter-spacing: 1px;
-}
-
-/* When active state */
-.active {
-  display: block !important;
-}
-
-.tabs .tabs-list li.active {
-  background-color: #c8c8cb !important;
-  color: black !important;
-}
-
-.active a {
-  color: black !important;
-}
-
-/* media query */
-@media screen and (max-width: 360px) {
-  .tabs {
-    margin: 0;
-    width: 150%;
-  }
-
-  .tabs .tabs-list li {
-    width: 80px;
-  }
+.container {
+  display: inline;
 }
 </style>
---
+++ components/tabs/tabs.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="tabs">
+      <ul>
+        <li
+          v-for="tab in tabs"
+          :key="tab.name"
+          :class="{ 'is-active': tab.isActive }"
+          @click="selectTab(tab)"
+        >
+          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="tabs-details">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { tabs: [] }
+  },
+  created() {
+    this.tabs = this.$children
+  },
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name === selectedTab.name
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tabs {
+  width: 60%;
+  height: auto;
+  margin: 0 auto;
+}
+
+.tabs li {
+  width: 150px;
+  float: left;
+  margin: 0 2px 0 0;
+  padding: 10px 5px;
+  text-align: center;
+  background-color: #fafafc;
+  border-radius: 3px;
+}
+
+.tabs li.is-active {
+  background-color: #c8c8cb !important;
+  color: black !important;
+}
+.tabs li a {
+  text-decoration: none;
+  color: #1c1b1b;
+}
+
+.tabs li:hover {
+  cursor: pointer;
+}
+.tabs ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.tabs a {
+  display: inline;
+  color: black !important;
+}
+
+@media screen and (max-width: 360px) {
+  .tabs {
+    margin: 0;
+    width: 150%;
+  }
+
+  .tabs li {
+    width: 80px;
+  }
+}
+</style>
