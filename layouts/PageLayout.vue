442a818bf14eca95df03aea9a0bd14be386c4b98
marziaf <maarzia.f@gmail.com>
2021-05-18 16:08:08 +0200

    home page optimized for mobile

--- layouts/PageLayout.vue
+++ layouts/PageLayout.vue
@@ -3,6 +3,7 @@
   import this to wrap your page in these
 -->
 
+<!--Show the pages wrapped between header and footer-->
 <template>
   <div>
     <the-topbar></the-topbar>
@@ -25,6 +26,21 @@ export default {
 <style>
 html {
   font-family: 'Roboto', sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+@media screen and (max-width: 1000px) {
+  html {
+    font-size: 16px;
+  }
+}
+@media screen and (min-width: 1000px) {
+  html {
+    font-size: 1.5vw;
+  }
 }
 
 a {
--- pages/index.vue
+++ pages/index.vue
@@ -35,21 +35,21 @@
           <ButtonWithText :title="'See more'" />
         </button>
       </div>
-      <div>
-        <div class="home-card">
-          <h1>About us</h1>
-          <p>Know more about our company</p>
-          <button class="but" @click="goTo('about')">
-            <ButtonWithText :title="'See more'" />
-          </button>
-        </div>
-        <div class="home-card">
-          <h1>Contact us</h1>
-          <p>Find our contact information</p>
-          <button class="but" @click="goTo('contact_us')">
-            <ButtonWithText :title="'See more'" />
-          </button>
-        </div>
+    </div>
+    <div class="row">
+      <div class="home-card">
+        <h1>About us</h1>
+        <p>Know more about our company</p>
+        <button class="but" @click="goTo('about')">
+          <ButtonWithText :title="'See more'" />
+        </button>
+      </div>
+      <div class="home-card">
+        <h1>Contact us</h1>
+        <p>Find our contact information</p>
+        <button class="but" @click="goTo('contact_us')">
+          <ButtonWithText :title="'See more'" />
+        </button>
       </div>
     </div>
   </div>
@@ -67,101 +67,85 @@ export default {
 </script>
 
 <style scoped>
+/** container wraps two rows */
 .container {
-  margin: 0 auto;
-  justify-content: center;
-  align-items: flex-start;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  flex-shrink: 0;
+}
+/** row made of columns */
+.row {
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: stretch;
+  margin-top: 2.5%;
+  margin-bottom: 2.5%;
+}
+/* column contains the sections */
+.column {
+  display: flex;
+  border-style: solid;
+  border-color: #999;
+  border-radius: 10px;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 1%;
+}
+
+/* home card contains the about and contact us sections */
+.home-card {
+  display: flex;
+  border-style: solid;
+  border-color: #999;
+  border-radius: 10px;
+  flex-direction: column;
+  justify-items: space-evenly;
+  align-items: center;
+  margin-bottom: 1%;
+}
+
+/* the size of the columns is based on the screen size */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+  .home-card {
+    width: 100%;
+  }
+}
+@media screen and (min-width: 600px) and (max-width: 992px) {
+  .column {
+    width: 45%;
+  }
+  .home-card {
+    width: 45%;
+  }
+}
+@media screen and (min-width: 992px) {
+  .column {
+    width: 30%;
+  }
+  .home-card {
+    width: 45%;
+  }
 }
 
 .but {
-  padding: 20px;
   background-color: transparent;
   border: transparent;
+  padding-top: 4%;
 }
 
 .but:focus {
   outline: none;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-
-.home-card {
-  height: auto;
-  width: 80%;
-  padding-right: 2.5%;
-  padding-left: 2.5%;
-  padding-top: 1%;
-  padding-bottom: 1%;
-  border-style: solid;
-  border-color: #999;
-  border-radius: 10px;
-  flex-direction: column;
-  justify-content: flex-start;
-  margin: 1%;
-}
-
-.home-card:hover {
-  color: #008cba;
-}
-
-.column {
-  display: grid;
-  height: auto;
-  width: 15%;
-  padding-right: 2%;
-  padding-left: 2%;
-  padding-top: 1%;
-  padding-bottom: 1%;
-  border-style: solid;
-  border-color: #999;
-  border-radius: 10px;
-  flex-direction: column;
-  justify-content: space-evenly;
-  margin: 0.5%;
-}
-
-.column:hover {
-  color: #008cba;
-}
-
-.row {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: stretch;
-}
-
-h1 {
-  padding-left: 1%;
-}
-
-p {
-  padding-left: 5%;
-  justify-content: left;
-  overflow: auto;
-  float: left;
-}
-
 img {
-  height: 90%;
   width: 90%;
-  padding-right: 2.5%;
-  padding-left: 2.5%;
-  padding-top: 1%;
-  padding-bottom: 1%;
-  margin: 1%;
-  justify-content: center;
 }
 </style>
